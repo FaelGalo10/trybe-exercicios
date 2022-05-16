35badffb28921b555204321ad30af05727f2942c
Rafael Ribeiro <rafaelfelipe.r@hotmail.com>
2022-05-16 15:31:13 -0300

    exercicios jaascript 1 e 2

---
+++ fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-1/exercicios.js
@@ -0,0 +1,64 @@
+/* const a = 10;
+const b = 5;
+
+console.log('Soma: ' + (a + b));
+console.log('Subtração: ' + (a - b));
+console.log('Multiplicação: ' + (a * b));
+console.log('Divisão: ' + (a / b));
+console.log('Módulo: ' + (a % b)); */
+
+
+/* const a = 35;
+const b = 10;
+
+if (a > b) {
+  console.log("'a' é maior que 'b'");
+}else {
+  console.log("'b' é maior que 'a'");
+} */
+
+/* const a = 20;
+const b = 10;
+const c = 40;
+
+if (a > b && a > c) {
+  console.log("'a' é maior que 'b' e 'c'");
+  
+} else if (b > a && b > c) {
+  console.log("'b' é maior que 'a' e 'c'");
+
+} else {
+  console.log('"c" ' +c+ ' é maior que "a" ' + a + ' e "b" ' + b +'.');
+}
+ */
+
+
+/* const number = 4;
+
+if (number > 0) {
+  console.log('positive');
+} else if (number < 0) {
+  console.log('negative');
+} else {
+  console.log('zero');
+}; */
+
+
+/* 
+let ladoA = 75;
+let ladoB = 80;
+let ladoC = 25;
+
+let somaDosAngulos = ladoA + ladoB + ladoC;
+
+if(ladoA > 0 && ladoB > 0 && ladoC > 0){
+  if (somaDosAngulos === 180) {
+    console.log(true);
+  } else {
+    console.log(false);
+  };
+} else {
+  console.log('Erro, ângulo 0 ou negativo');
+}
+ */
+
