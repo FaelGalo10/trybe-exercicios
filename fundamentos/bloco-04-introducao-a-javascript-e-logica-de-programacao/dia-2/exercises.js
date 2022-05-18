a01b7683891af6156e5eee9d146dd067f362172f
Rafael Ribeiro <rafaelfelipe.r@hotmail.com>
2022-05-18 00:14:17 -0300

    realizando exercicios dia 2 javascript

---
+++ fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-2/exercises.js
@@ -0,0 +1,25 @@
+/* let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+
+for (let index = 0; index < numbers.length; index += 1) {
+  console.log(numbers[index]);
+} */
+
+/* let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+let result = 0;
+
+for (let index = 0; index < numbers.length; index += 1) {
+  result += numbers[index];
+}
+
+console.log(result); */
+
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+let sum = 0;
+
+for (let index = 0; index < numbers.length; index += 1) {
+  sum += numbers[index];
+}
+
+let average = sum / numbers.length;
+
+console.log(average);
